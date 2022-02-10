9cf6db8fe1423edb1f0d87fd06b6833a87c57571
Northy11 <f20201696@pilani.bits-pilani.ac.in>
2022-02-10 18:55:51 +0530

    Imported assets

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    private int score;
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame(int difficulty)
+    {
+        score = 0;
+        isGameActive = true;
+        spawnRate /= difficulty;
+        UpdateScore(0);
+        StartCoroutine(SpawnTarget());
+        titleScreen.SetActive(false);
+
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void UpdateScore(int ScoretoAdd)
+    {
+        scoreText.text = "Score:" + score;
+        score += ScoretoAdd;
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+        yield return new WaitForSeconds(spawnRate);
+        int index = Random.Range(0, targets.Count);
+        Instantiate(targets[index]);
+        }
+
+    }
+
+}
